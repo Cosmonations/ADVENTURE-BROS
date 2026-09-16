80b0f5d7ca4aae62f0280bdcd5b7504438678ef4
Charles <chuckansong@gmail.com>
2026-09-16 16:01:29 -0400

    Replace merchant ID and API key with placeholders

--- return.php
+++ return.php
@@ -6,8 +6,8 @@ $orderId = $_GET['order-id'] ?? null;
 
 if (!empty($token) && !empty($orderId)) {
     
-    $merchantId = '042624216522'; 
-    $apiKey     = 'u7HL2aenabmiiPcL96czt-sXU0wvcqfTMwQVmw6kUx-u7RdFILFvNBeFG7foKsO-x6MJarUsaH768pZP68A';   
+    $merchantId = 'MERCHANT ID'; 
+    $apiKey     = 'API KEY';   
 
     $queryPayload = [
         'merchant-id' => $merchantId,
